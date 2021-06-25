c7b47d8109f8d61c58d91c0d1ae70e3a3c0f40bc
Matheus Pellisoli <pellisolimatheus@gmail.com>
2021-06-25 18:41:59 -0300

    wip

--- out/infra/repository/itemRepositoryImp.js
+++ out/infra/repository/itemRepositoryImp.js
@@ -6,12 +6,10 @@ var tag_1 = require("../../domain/Model/tag");
 var tagType_1 = require("../../domain/Model/tagType");
 var ItemRepositoryImp = /** @class */ (function () {
     function ItemRepositoryImp() {
-        this.tags = [new tag_1.Tag("a6123396-e45b-4f68-96aa-f4ce72e1d933", "Azul", new tagType_1.TagType("cca10c75-6728-452f-8843-7a4425f3dfc1", "Cor", true, false, "", true), true),
-            new tag_1.Tag("03f6b13c-6e6b-4b8a-bb1f-8cfc903bcd59", "Vermelho", new tagType_1.TagType("cca10c75-6728-452f-8843-7a4425f3dfc1", "Cor", true, false, "", true), true),
-            new tag_1.Tag("76805e39-f653-4f7b-9fa5-0ff795cac5fe", "G", new tagType_1.TagType("bf97d25a-36b9-40d7-83f1-809469de3ce4", "Tamanho", true, true, "P,M,G,GG", true), true)];
-        this.imgs = ["https://cdn.auth0.com/blog/whatabyte/burger-sm.png"];
+        this.tags = [new tag_1.Tag("a6123396-e45b-4f68-96aa-f4ce72e1d933", "Chaves", new tagType_1.TagType("cca10c75-6728-452f-8843-7a4425f3dfc1", "tipo", true, false, "", true), true),];
+        this.imgs = ["https://img.lojadomecanico.com.br/IMAGENS/2/301/110860/Jogo-100-pecas-Chaves-de-Fenda-Phillips--fortgpro-fg81931.JPG"];
         this.items = [
-            new item_1.Item("18ec39f5-c2c8-4a95-9931-38482dcd48f7", "casaco", "um belo casaco", 149.99, this.tags, this.imgs, true)
+            new item_1.Item("18ec39f5-c2c8-4a95-9931-38482dcd48f7", "Jogo 100 Chaves", "O Jogo com 100 de Chaves de Fenda e Phillips FORTG PRO FG8193", 149.99, this.tags, this.imgs, true)
         ];
     }
     ItemRepositoryImp.prototype.findAll = function () {
